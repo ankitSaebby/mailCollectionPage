8f9edfe237a39a765bd7ef553214e4e1999bd59c
Ankit Singh <54979627+ankitSaebby@users.noreply.github.com>
2020-07-03 12:41:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const express= require("express");
+const bodyParser= require("body-parser");
+const request =require("request");
+
+const app=express();
+app.listen(3000,function(){
+  console.log("server is running on port 3000");
+});
